749105e611350e69e7e5f955f9dfdd7794fc1b57
Ubuntu <ubuntu@ip-172-31-29-95.ec2.internal>
2023-04-13 16:10:14 +0000

    stats for redis

---
+++ caliper/save_docker_stats.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+other_script_pid=$1
+output_file="output/docker_stats.csv"
+
+# Write the headers to the output file
+echo "Container, CPU %, Memory Usage" > "$output_file"
+
+while true; do
+  # Check if the other script is still running
+  if ! ps -p "$other_script_pid" > /dev/null; then
+    break
+  fi
+
+  # Get the stats in CSV format
+  stats=$(docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}}")
+
+  # Append the stats to the output file
+  echo "$stats" | tail -n +2 >> "$output_file"
+
+  # Wait for 30 seconds
+  sleep 30
+done
--- redis/script.sh
+++ redis/script.sh
@@ -4,31 +4,56 @@ echo "Starting Docker Stats..."
 ./save_docker_stats.sh $$ &
 
 echo "Starting Redis Benchmark using YCSB..."
-docker-compose -f docker-compose.yml up --scale redis-master=1 --scale redis-replica=5 -d
+docker-compose -f docker-compose.yml up --scale redis-master=1 --scale redis-replica=4 -d
 
 cd /home/admin/YCSB/
 
 # Run benchmarks 3 times for each workload
 workloads="/home/admin/YCSB/workloads"
 
-mkdir -p /home/admin/output/
+#mkdir -p /home/admin/output/
+sudo chown :999 /home/admin/output/
+sudo chmod 770 /home/admin/output/
 load_output="/home/admin/output/load_output.csv"
 run_output="/home/admin/output/run_output.csv"
 
-for workload in "$workloads"/workload*[a-f]
+for i in {1..3}
 do
-	if [[ -f "$workload" ]]; then
-		for i in {1..3}
-		do
-			printf "\n##################################################################################\n" >> $load_output
-			printf "Loading data $(basename "$workload") try $i \n" >> $load_output
-			./bin/ycsb load redis -s -P $workload -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $load_output
-			printf "\n##################################################################################\n" >> $run_output
-			printf "Running test $(basename "$workload") try $i\n" >> $run_output
-			./bin/ycsb run redis -s -P $workload -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $run_output
-		done
-	fi
-done
+printf "\n##################################################################################\n" >> $load_output
+printf "Loading data worload A try $i \n" >> $load_output
+./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $load_output
+printf "\n##################################################################################\n" >> $run_output
+printf "Running test workoad A try $i\n" >> $run_output
+./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $run_output
+
+printf "\n##################################################################################\n" >> $load_output
+printf " Loading data worload B try $i \n" >> $load_output
+./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $load_output
+printf "\n##################################################################################\n" >> $run_output
+printf "Running test workoad B try $i\n" >> $run_output
+./bin/ycsb run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $run_output
+
+printf "\n##################################################################################\n" >> $load_output
+printf "Loading data worload C try $i\n" >> $load_output
+./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $load_output
+printf "\n##################################################################################\n" >> $run_output
+printf "Running test workoad C try $i\n" >> $run_output
+./bin/ycsb run redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $run_output
+
+#for workload in "$workloads"/workload*[a-c]
+#do
+#	if [[ -f "$workload" ]]; then
+#		for i in {1..3}
+#		do
+#			printf "\n##################################################################################\n" >> $load_output
+#			printf "Loading data $(basename "$workload") try $i \n" >> $load_output
+#			./bin/ycsb load redis -s -P $workload -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $load_output
+#			printf "\n##################################################################################\n" >> $run_output
+#			printf "Running test $(basename "$workload") try $i\n" >> $run_output
+#			./bin/ycsb run redis -s -P $workload -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> $run_output
+#		done
+#	fi
+#done
 
 docker-compose -f docker-compose.yml down -v
 echo "Finished Redis Benchmark"
